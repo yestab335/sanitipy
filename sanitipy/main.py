1772375ccde27d26e05236af57543d06f7ed1765
Adam <yestab335@gmail.com>
2025-04-10 08:00:02 -0400

    Temporary main file

---
+++ sanitipy/main.py
@@ -0,0 +1,8 @@
+def AutoClean():
+  print('This project has just started!')
+  print('It is actively being worked on...')
+  print('As of right now I have only published this project to reserve the name')
+  print('Sorry for the inconvience')
+
+if __name__ == '__main__':
+  AutoClean()
