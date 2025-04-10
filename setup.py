48c84db912d3677e05e8f55708f39e4b8f9a501d
Adam <yestab335@gmail.com>
2025-04-10 09:14:34 -0400

    Setup file for PyPi

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='sanitipy',
+  version='0.1',
+  packages=find_packages(),
+  install_requires=[
+    # Add dependencies here.
+  ],
+)
