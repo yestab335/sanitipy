4b692dae71aea5ee5f5dd0402a604c0a19ad17c4
Adam <yestab335@gmail.com>
2025-04-17 09:19:00 -0400

    Removed file because it was not following standard packaging requirements. All of the file contents have been movied to the new file sanitipy.py

--- sanitipy/main.py
+++
@@ -1,8 +0,0 @@
-def AutoClean():
-  print('This project has just started!')
-  print('It is actively being worked on...')
-  print('As of right now I have only published this project to reserve the name')
-  print('Sorry for the inconvience')
-
-if __name__ == '__main__':
-  AutoClean()
