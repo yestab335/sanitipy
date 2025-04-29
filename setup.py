bfa21c3877554603436ff1b2198e2b1ceda48be2
Adam <yestab335@gmail.com>
2025-04-29 15:59:21 -0400

    Setup file for PyPi

--- setup.py
+++ setup.py
@@ -2,9 +2,28 @@ from setuptools import setup, find_packages
 
 setup(
   name='sanitipy',
-  version='0.1',
+  description='',
+  author='Adam Ben-Aamr',
+  author_email='adambenaamr@gmail.com',
+
+  # Note: Always update the version number
+  version='0.1.1',
   packages=find_packages(),
   install_requires=[
     # Add dependencies here.
+    'pandas',
+    'numpy',
+    'scikit-learn',
+    'matplotlib',
+  ],
+  url='https://github.com/yestab335/sanitipy',
+  download_url='',
+  keywords=[
+    'data',
+    'data cleaning',
+    'missing data',
+    'data science',
+    'pandas',
+    'python'
   ],
 )
