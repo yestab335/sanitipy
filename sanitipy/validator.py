186ac5bf61a232bdafa64d846a51e259d7ad33f5
Adam <yestab335@gmail.com>
2025-08-01 00:22:25 -0400

    Added return types to functions. Added type checking comments for the new VSCode type checking extension update. Simplified fuctions to be more pythonic

--- sanitipy/validator.py
+++ sanitipy/validator.py
@@ -1,21 +1,20 @@
 import pandas as pd
 
 class Validator:
-  def __init__(self, data: pd.DataFrame):
-    self.data = data
-  
-  def validate_data_types(self):
+  def validate_data_types(self) -> bool:
     # Check for consistent data types
-    if self.empty:
+    if self.empty: # type: ignore
       return True
     
-    data_types = self.dtypes
+    data_types = self.dtypes # type: ignore
 
     if data_types.nunique() == 1:
       return True
     else:
       return False
     
-  def check_missing_values(self):
+  def check_missing_values(self, data) -> int:
+    data = data
+
     # Identify missing values in the dataset
-    return self.data.isnull().sum()
+    return data.isnull().sum()
