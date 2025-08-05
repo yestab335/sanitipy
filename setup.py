bbe77598de8709e6a2a1fd43cdbbcd4336814933
Adam <yestab335@gmail.com>
2025-08-05 17:11:29 -0400

    Updated project version number and added a project description

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
   name='sanitipy',
-  description='',
+  description='Sanitipy is a powerful and user-friendly Python library designed for data cleaning and preprocessing. It provides essential utilities to streamline the process of preparing datasets for analysis or modeling. With features such as duplicate removal, handling missing values, and automatic data type inference, sanitipy simplifies the data cleaning workflow, making it an invaluable tool for data scientists and analysts.',
   author='Adam Ben-Aamr',
   author_email='adambenaamr@gmail.com',
 
   # Note: Always update the version number
-  version='0.1.1',
+  version='1.0.0',
   packages=find_packages(),
   install_requires=[
     # Add dependencies here.
