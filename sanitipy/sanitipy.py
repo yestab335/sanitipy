582227cd56dcbb6e0554a58725132efafc4c1b38
Adam <yestab335@gmail.com>
2025-08-05 17:04:31 -0400

    Cleaned up code and improved logic

--- sanitipy/sanitipy.py
+++ sanitipy/sanitipy.py
@@ -3,27 +3,28 @@ from .validator import Validator
 from .preprocessor import Preprocessor
 
 class DataCleaner:
-  def __init__(self, data: pd.DataFrame):
-    self.data = data
-    self.validator = Validator(data)
-    self.preprocessor = Preprocessor(data)
-  
-  def help(self):
-    print('More information can be found on the offical docs')
-    print('Link: https://link.netlify.app')
-  
-  def clean_data(self):
-    self.validator.check_missing_values()
-    self.validator.remove_na()
-    self.validator.remove_duplicates()
+  def __init__(self):
+    # Create an instance of the Validator
+    self.validator = Validator()
 
-    return self.data
-  
-  def AutoClean():
-    print('This project has just started!')
-    print('It is actively being worked on...')
-    print('As of right now I have only published this project to reserve the name')
-    print('Sorry for the inconvience')
+    # Create an instance of the Preprocessor
+    self.preprocessor = Preprocessor()
 
-if __name__ == '__main__':
-  DataCleaner()
+  def clean_data(self, data_frame: pd.DataFrame) -> pd.DataFrame:
+    missing_values: int = self.validator.check_missing_values(data_frame)
+    validate_data_types: bool = self.validator.validate_data_types()
+    data_frame = self.preprocessor.remove_duplicates(data_frame)
+
+    if missing_values > 0:
+      data_frame = self.preprocessor.remove_na(data_frame)
+      remaining_missing_values: int = self.validator.check_missing_values(data_frame)
+
+      if remaining_missing_values != 0:
+        raise ValueError(f'Data still contains {remaining_missing_values} missing values. Check the Data Set manually')
+
+    if not validate_data_types:
+      data_frame = self.preprocessor.infer_data_types(data_frame)
+
+    data_frame = data_frame.reset_index(drop=True)
+
+    return data_frame
