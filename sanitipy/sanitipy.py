5e4884303270ec252891b4ac6a97613ad977d7aa
Adam <yestab335@gmail.com>
2025-08-05 21:32:48 -0400

    Refactored code so that the Validator class takes an input on initialization. Moved the data_frame variable into global scope to clean up clean_data() function

--- sanitipy/sanitipy.py
+++ sanitipy/sanitipy.py
@@ -3,28 +3,31 @@ from .validator import Validator
 from .preprocessor import Preprocessor
 
 class DataCleaner:
-  def __init__(self):
+  def __init__(self, data_frame: pd.DataFrame):
     # Create an instance of the Validator
-    self.validator = Validator()
+    self.validator = Validator(data_frame)
 
     # Create an instance of the Preprocessor
     self.preprocessor = Preprocessor()
 
-  def clean_data(self, data_frame: pd.DataFrame) -> pd.DataFrame:
-    missing_values: int = self.validator.check_missing_values(data_frame)
+    # Store the data_frame variable
+    self.data_frame = data_frame
+
+  def clean_data(self) -> pd.DataFrame:
+    missing_values: int = self.validator.check_missing_values(self.data_frame)
     validate_data_types: bool = self.validator.validate_data_types()
-    data_frame = self.preprocessor.remove_duplicates(data_frame)
+    self.data_frame = self.preprocessor.remove_duplicates(self.data_frame)
 
     if missing_values > 0:
-      data_frame = self.preprocessor.remove_na(data_frame)
-      remaining_missing_values: int = self.validator.check_missing_values(data_frame)
+      self.data_frame = self.preprocessor.remove_na(self.data_frame)
+      remaining_missing_values: int = self.validator.check_missing_values(self.data_frame)
 
       if remaining_missing_values != 0:
         raise ValueError(f'Data still contains {remaining_missing_values} missing values. Check the Data Set manually')
 
     if not validate_data_types:
-      data_frame = self.preprocessor.infer_data_types(data_frame)
+      self.data_frame = self.preprocessor.infer_data_types(self.data_frame)
 
-    data_frame = data_frame.reset_index(drop=True)
+    self.data_frame = self.data_frame.reset_index(drop=True)
 
-    return data_frame
+    return self.data_frame
