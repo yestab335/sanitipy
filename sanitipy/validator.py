232691fe89b2a9b4182d6042ba6ee20c9ccd66d8
Adam <yestab335@gmail.com>
2025-08-05 21:31:30 -0400

    Refactored so that a dataset needs to get passed through. Fixed the missing self and dtype errors. Added data type to check_missing_values function. Added a double summation for all aspects of a dataset

--- sanitipy/validator.py
+++ sanitipy/validator.py
@@ -1,18 +1,25 @@
+import pandas as pd
+
 class Validator:
+  def __init__(self, data: pd.DataFrame) -> None:
+    self.data = data
+    self.empty = self.check_missing_values(data) > 0
+
   def validate_data_types(self) -> bool:
     # Check for consistent data types
-    if self.empty: # type: ignore
+    if self.empty:
       return True
     
-    data_types = self.dtypes # type: ignore
+    data_types = self.data.dtypes
 
     if data_types.nunique() == 1:
       return True
     else:
       return False
     
-  def check_missing_values(self, data) -> int:
-    data = data
+  def check_missing_values(self, data: pd.DataFrame) -> int:
+    if data is None:
+      return 0
 
     # Identify missing values in the dataset
-    return data.isnull().sum()
+    return data.isnull().sum().sum()
