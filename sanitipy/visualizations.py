14a1caceb5cff48bc3b4370b56905eb377e20310
Adam <yestab335@gmail.com>
2025-09-04 14:47:51 -0400

    Added DOCSTRING explanations

--- sanitipy/visualizations.py
+++ sanitipy/visualizations.py
@@ -4,10 +4,48 @@ import missingno as msno
 from .validator import Validator
 
 class Visualizations(Validator):
+  """
+  Provides visualization utilities for exploring a dataset, 
+  with a focus on missing values.
+  
+  This class extends the `Validator` class and adds methods to 
+  graphically represent missing data in a pandas DataFrame.
+
+  Parameters:
+  -----------
+  data: pd.DataFrame
+  - The dataset to be validated and visualized.
+
+  Methods:
+  --------
+  show_missing_values()
+  - Displays a bar chart of missing values per column in the dataset.
+  """
   def __init__(self, data: pd.DataFrame) -> None:
     super().__init__(data)
 
   def show_missing_values(self) -> None:
+    """
+    Display a bar chart of missing values in the dataset.
+
+    The chart shows the number of non-missing (observed) values
+    for each column in the DataFrame, helping to identify the
+    extent and distribution of missing data.
+
+    Parameters:
+    -----------
+    None
+    - This method is not supposed to take any type of parameters.
+
+    Returns:
+    --------
+    None
+    - This method produces a matplotlib plot and does not return any value.
+
+    See Also:
+    ---------
+    missingno.bar: Function from the `missingno` package used to generate the missing values bar chart.
+    """
     data_frame = self.data
     
     plt.title('Missing Values in Dataset')
