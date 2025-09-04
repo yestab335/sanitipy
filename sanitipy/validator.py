2e0d84178046f7d3f074c13a7e116e83069493ef
Adam <yestab335@gmail.com>
2025-09-04 14:57:27 -0400

    Added DOCSTRING explanations

--- sanitipy/validator.py
+++ sanitipy/validator.py
@@ -1,11 +1,44 @@
 import pandas as pd
 
 class Validator:
+  """
+  A utility class for validating and inspecting pandas DataFrames.
+
+  This class provides methods to check for missing values and
+  validate the consistency of data types across a dataset.
+
+  Parameters:
+  -----------
+  data: pd.DataFrame
+  - The dataset to be validated.
+
+  Attributes:
+  -----------
+  data: pd.DataFrame
+  - The input dataset.
+
+  empty: bool
+  - Indicates whether the dataset contains missing values.
+  """
   def __init__(self, data: pd.DataFrame) -> None:
     self.data = data
     self.empty = self.check_missing_values(data) > 0
 
   def validate_data_types(self) -> bool:
+    """
+    Validate whether the dataset has consistent data types.
+
+    Parameters:
+    -----------
+    None
+    - This function does not take any parameters.
+
+    Returns:
+    --------
+    bool
+    - True if the dataset contains missing values or if all
+    columns share the same data type, False otherwise.
+    """
     # Check for consistent data types
     if self.empty:
       return True
@@ -18,6 +51,19 @@ class Validator:
       return False
     
   def check_missing_values(self, data: pd.DataFrame) -> int:
+    """
+    Count the total number of missing values in the dataset.
+
+    Parameters:
+    -----------
+    data: pd.DataFrame
+    - The dataset in which to check for missing values
+
+    Returns:
+    --------
+    int
+    - The total number of missing values across the entire dataset.
+    """
     if data is None:
       return 0
 
